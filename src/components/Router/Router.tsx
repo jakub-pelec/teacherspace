76e685b19876cd375da2d67ae1c85547ece17c37
jakub-pelec <kuba1pelec@gmail.com>
2021-10-15 02:39:07 +0200

    protected dashboard route

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,22 +1,31 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 import LoginPage from "../LoginPage/LoginPage";
 import RegisterPage from "../RegisterPage/RegisterPage";
 import Dashboard from "../Dashboard/Dashboard";
 import SignupSecondStage from "../SignupSecondStage/SignupSecondStage";
 import LandingPage from "../LandingPage/LandingPage";
 
-const Router = () => {
+interface IProps {
+	isLoggedIn: boolean;
+}
+
+const Router: React.FC<IProps> = ({ isLoggedIn }) => {
 	return (
 		<BrowserRouter>
 			<Switch>
 				<Route exact path="/" component={LandingPage} />
 				<Route exact path="/login" component={LoginPage} />
 				<Route exact path="/register" component={RegisterPage} />
-				<Route exact path="/dashboard" component={Dashboard} />
 				<Route exact path="/second-stage" component={SignupSecondStage} />
+				{isLoggedIn && <Route exact path="/dashboard" component={Dashboard} />}
 			</Switch>
 		</BrowserRouter>
 	);
 };
 
-export default Router;
+const mapStateToProps = (state: any) => ({
+	isLoggedIn: !!state.auth.firestoreID,
+});
+
+export default connect(mapStateToProps)(Router);
