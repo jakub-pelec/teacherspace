c6f6ab8ac04e36ba960b94b5a1eb17f84381d8ed
cichowlasp <cichowlas.p@gmail.com>
2021-10-18 13:33:52 +0200

    pushing unsaved file

--- src/components/ShowNoteView/Elements.tsx
+++ src/components/ShowNoteView/Elements.tsx
@@ -4,16 +4,6 @@ interface StyledProps {
 	open: boolean;
 }
 
-export const Background = styled.div`
-	position: fixed;
-	top: 0;
-	left: 0em;
-	right: 0;
-	bottom: 0;
-	content: " ";
-	background: rgba(0, 0, 0, 0.5);
-`;
-
 const SlideIn = keyframes`
  0% { transform: translate(-50%, -200%); }
 
